b3748d0645ee694da34becf450acfd9f2d92a619
allllfo <mirae4292@gmail.com>
2024-06-20 18:24:37 +0900

    feat : allowance child DTO 생성

---
+++ isol/server-child/src/main/java/shinhan/server_child/domain/allowance/dto/UnAcceptTemporalAllowanceFindAllResponse.java
@@ -0,0 +1,26 @@
+package shinhan.server_child.domain.allowance.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shinhan.server_child.domain.allowance.entity.TemporalAllowance;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UnAcceptTemporalAllowanceFindAllResponse {
+    private int amount;
+    private String name;
+    private LocalDate createDate;
+
+    public static UnAcceptTemporalAllowanceFindAllResponse of(TemporalAllowance temporalAllowance, String name){
+        return UnAcceptTemporalAllowanceFindAllResponse.builder()
+                .amount(temporalAllowance.getPrice())
+                .name(name)
+                .build();
+    }
+}
