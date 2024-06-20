9bc6a237888b5fa8ecb370ab993b279f04571b4a
kim hyun soo <hyjkl0124@naver.com>
2024-06-21 02:54:46 +0900

    feat everyChild

--- isol/server-child/src/main/java/shinhan/server_child/domain/invest/controller/InvestController.java
+++ isol/server-child/src/main/java/shinhan/server_child/domain/invest/controller/InvestController.java
@@ -2,17 +2,14 @@ package shinhan.server_child.domain.invest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import shinhan.server_child.domain.invest.dto.InvestStockRequest;
 import shinhan.server_child.domain.invest.dto.PortfolioResponse;
 import shinhan.server_child.domain.invest.service.InvestProposalService;
 import shinhan.server_child.domain.invest.service.InvestService;
-import shinhan.server_common.global.utils.ApiResult;
 import shinhan.server_common.global.utils.ApiUtils;
 import static shinhan.server_common.global.utils.ApiUtils.success;
 @RestController
--- isol/server-child/src/main/java/shinhan/server_child/domain/invest/dto/PortfolioResponse.java
+++ isol/server-child/src/main/java/shinhan/server_child/domain/invest/dto/PortfolioResponse.java
@@ -2,12 +2,13 @@ package shinhan.server_child.domain.invest.dto;
 
 import java.util.List;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class PortfolioResponse {
     int totalEvaluationAmount;
     int totalPurchaseAmount;
     double totalProfit;
     List<InvestTradeDetailResponse> investTradeList;
-
 }
