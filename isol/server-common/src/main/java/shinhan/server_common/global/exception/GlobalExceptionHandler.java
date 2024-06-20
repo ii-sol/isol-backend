7bb8379fc63b8402beb9c67b30a11f8f11ae2e7c
ye-s-rin <pyr53540@gmail.com>
2024-06-20 15:49:52 +0900

    fix: use HttpServletResponse

--- isol/server-common/src/main/java/shinhan/server_common/global/exception/GlobalExceptionHandler.java
+++ isol/server-common/src/main/java/shinhan/server_common/global/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.io.DecodingException;
 import io.jsonwebtoken.lang.UnknownClassException;
 import io.jsonwebtoken.security.WeakKeyException;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.UnexpectedTypeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.ConverterNotFoundException;
@@ -30,97 +31,113 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ApiResult<String> handleAuthException(AuthException error) {
+    public ApiResult<String> handleAuthException(AuthException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
         return error(error.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ApiResult<String> handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException error) {
+    public ApiResult<String> handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.CONFLICT.value());
         return error(error.getMessage(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiResult<String> handleNoSuchElementException(NoSuchElementException error) {
+    public ApiResult<String> handleNoSuchElementException(NoSuchElementException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.NOT_FOUND.value());
         return error(error.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiResult<String> handleSQLException(SQLException error) {
+    public ApiResult<String> handleSQLException(SQLException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         return error(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiResult<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException error) {
+    public ApiResult<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
         return error(error.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiResult<String> handleConverterNotFoundException(ConverterNotFoundException error) {
+    public ApiResult<String> handleConverterNotFoundException(ConverterNotFoundException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         return error(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiResult<String> handleUnknownClassException(UnknownClassException error) {
+    public ApiResult<String> handleUnknownClassException(UnknownClassException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         return error(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiResult<String> handleWeakKeyException(WeakKeyException error) {
+    public ApiResult<String> handleWeakKeyException(WeakKeyException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         return error(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiResult<String> handleDecodingException(DecodingException error) {
+    public ApiResult<String> handleDecodingException(DecodingException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         return error(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiResult<String> handleUnexpectedTypeException(UnexpectedTypeException error) {
+    public ApiResult<String> handleUnexpectedTypeException(UnexpectedTypeException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         return error(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-    public ApiResult<String> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException error) {
+    public ApiResult<String> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value());
         return error(error.getMessage(), HttpStatus.UNSUPPORTED_MEDIA_TYPE);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ApiResult<String> handleMalformedJwtException(MalformedJwtException error) {
+    public ApiResult<String> handleMalformedJwtException(MalformedJwtException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
         return error(error.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ApiResult<String> handleExpiredJwtException(ExpiredJwtException error) {
+    public ApiResult<String> handleExpiredJwtException(ExpiredJwtException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
         return error(error.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiResult<String> handleJsonProcessingException(JsonProcessingException error) {
+    public ApiResult<String> handleJsonProcessingException(JsonProcessingException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         return error(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ApiResult<String> handleServletException(ServletException error) {
+    public ApiResult<String> handleServletException(ServletException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.FORBIDDEN.value());
         return error(error.getMessage(), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiResult<String> handleNullPointerException(NullPointerException error) {
+    public ApiResult<String> handleNullPointerException(NullPointerException error, HttpServletResponse response) {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         return error(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
