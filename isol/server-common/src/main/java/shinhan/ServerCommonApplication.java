f01dbca97ffa403d63d55e771852d0babcbede57
kim hyun soo <hyjkl0124@naver.com>
2024-06-21 08:00:49 +0900

    feat cors

--- isol/server-common/src/main/java/shinhan/ServerCommonApplication.java
+++ isol/server-common/src/main/java/shinhan/ServerCommonApplication.java
@@ -2,21 +2,36 @@ package shinhan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import shinhan.server_common.notification.mongo.NotificationRepository;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import shinhan.server_common.notification.mongo.UserRepository;
 
 // common
 @SpringBootApplication
 @EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(
     type = FilterType.ASSIGNABLE_TYPE,
-    classes = NotificationRepository.class))
+    classes = UserRepository.class))
 @EnableMongoRepositories(basePackages = {"shinhan.server_common.notification.mongo"})
 public class ServerCommonApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerCommonApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowCredentials(true);
+            }
+        };
     }
 }
