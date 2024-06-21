f0ff077b9d6ec7aa660e4b3a8432b2383eaf15de
allllfo <mirae4292@gmail.com>
2024-06-21 14:20:18 +0900

    feat : child allowance api 생성

---
+++ isol/server-child/src/main/java/shinhan/server_child/domain/allowance/repository/ChildMonthlyAllowanceRepository.java
@@ -0,0 +1,12 @@
+package shinhan.server_child.domain.allowance.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import shinhan.server_child.domain.allowance.entity.ChildMonthlyAllowance;
+
+import java.util.List;
+
+public interface ChildMonthlyAllowanceRepository extends JpaRepository<ChildMonthlyAllowance, Integer>, ChildMonthlyAllowanceRepositoryCustom {
+
+
+    List<ChildMonthlyAllowance> findByChildSerialNumberAndStatus(Long serialNumber, Integer status);
+}
