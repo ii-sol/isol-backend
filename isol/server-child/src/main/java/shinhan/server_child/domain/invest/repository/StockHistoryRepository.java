0305592404c2fe19cec0550103d98bffc03bfe1e
ye-s-rin <pyr53540@gmail.com>
2024-06-22 07:20:59 +0900

    test: temp delete

--- isol/server-child/src/main/java/shinhan/server_child/domain/invest/repository/StockHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package shinhan.server_child.domain.invest.repository;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import shinhan.server_child.domain.invest.entity.StockHistory;
-
-public interface StockHistoryRepository extends JpaRepository<StockHistory,Integer> {
-    List<StockHistory> findByAccountNum(String accountNum);
-}
--- isol/server-common/src/main/java/shinhan/server_common/domain/account/repository/AccountHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package shinhan.server_common.domain.account.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import shinhan.server_common.domain.account.entity.AccountHistory;
-
-public interface AccountHistoryRepository extends JpaRepository<AccountHistory, Integer>, AccountHistoryRepositoryCustom{
-
-}
