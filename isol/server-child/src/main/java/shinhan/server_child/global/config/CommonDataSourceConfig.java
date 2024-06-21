5ae614adb104947dc7c9b42255a4157aead821f6
ye-s-rin <pyr53540@gmail.com>
2024-06-21 19:26:24 +0900

    feat: 공통 DB config

---
+++ isol/server-child/src/main/java/shinhan/server_child/global/config/CommonDataSourceConfig.java
@@ -0,0 +1,63 @@
+package shinhan.server_child.global.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableConfigurationProperties(DataSourceProperties.class)
+@EnableJpaRepositories(
+        basePackages = "shinhan.server_common.domain.user.repository",
+        entityManagerFactoryRef = "commonEntityManagerFactory",
+        transactionManagerRef = "commonTransactionManager")
+public class CommonDataSourceConfig {
+
+    @Value("${COMMON_DB_URL}")
+    private String dbUrl;
+    @Value("${COMMON_DB_USERNAME}")
+    private String dbUsername;
+    @Value("${COMMON_DB_PASSWORD}")
+    private String dbPassword;
+
+    @Bean
+    @Primary
+    public DataSource commonDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean commonEntityManagerFactory(
+            @Qualifier("commonDataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("shinhan.server_common.domain.user.entity");
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager commonTransactionManager(
+            @Qualifier("commonDataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
