2cc5ac55c125897376bc773a59bba4ec82b61361
allllfo <mirae4292@gmail.com>
2024-06-21 15:01:27 +0900

    feat: LocalDateTime을 Timestamp로 바꾸는 config 추가

--- isol/server-common/src/main/java/shinhan/server_common/global/config/ObjectMapperConfig.java
+++ isol/server-common/src/main/java/shinhan/server_common/global/config/ObjectMapperConfig.java
@@ -2,6 +2,8 @@ package shinhan.server_common.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +18,8 @@ public class ObjectMapperConfig {
         objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE);
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         objectMapper.setDateFormat(dateFormat);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
         return objectMapper;
     }
 }
