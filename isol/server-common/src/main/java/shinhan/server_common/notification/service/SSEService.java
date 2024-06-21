039dd44259f6c650cb404185d9ecf8b3c6be0233
Future <86925185+allllfo@users.noreply.github.com>
2024-06-21 10:26:26 +0900

    feat : allowance관련 api 생성 (정기용돈 제외) (#18)
    * refactor : userUtils 메소드 이름 변경
    
    * feat : allowance repository, entity, service 생성
    
    * feat : allowance 관련 dto 생성
    
    * Revert "feat : allowance 관련 dto 생성"
    
    This reverts commit 51246392564ae61199c998e88309f0f2f71b9c49.
    
    * feat : allowance관련 dto 생성
    
    * Revert "Merge branch 'develop' into feature/11-child-allowance"
    
    This reverts commit c2e787bb413998ba357038ca063e2cc4a9525eb5, reversing
    changes made to 4cda517082ca80a05a4717da0af371ce6201e3a5.
    
    * Revert "Merge branch 'develop' into feature/11-child-allowance"
    
    This reverts commit 4cda517082ca80a05a4717da0af371ce6201e3a5, reversing
    changes made to 51246392564ae61199c998e88309f0f2f71b9c49.
    
    * feat : allowance child DTO 생성
    
    * refactor : 매개변수를 tempUser에서 userSerialNumber로 변경 및 함수 이름 변경
    
    * feat : 부모 용돈 관련 api 생성 ( entity, service, repository , dto )
    
    * refactor : child 용돈 관련 이름 변경 ( 앞에 전부 Child붙임 )
    
    * refactor : child 이름 변경하면서 생긴 dto내 이름 변경
    
    * Revert "Merge branch 'develop' into feature/11-allowance-exceptMonthly"
    
    This reverts commit 802769d6821ca42edbd640999a477ea09a0b259e, reversing
    changes made to a2227ad96c29440fe82b3e7e556e9239c59fb4d2.

--- isol/server-common/src/main/java/shinhan/server_common/notification/service/SSEService.java
+++ isol/server-common/src/main/java/shinhan/server_common/notification/service/SSEService.java
@@ -83,7 +83,7 @@ public class SSEService {
 
     //private : 알림 저장하기
     private Notification saveNotification(Long serialNumber, String senderName, Integer functionCode, String message){
-        TempUser receiver = userUtils.getUser(serialNumber);
+        TempUser receiver = userUtils.getUserBySerialNumber(serialNumber);
 
         Notification notification = Notification.builder()
                 .receiverSerialNumber(serialNumber)
