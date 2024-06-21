cd57a347bf43e0af5000e815c783b0de26b11614
jkl0124 <122847760+jkl0124@users.noreply.github.com>
2024-06-21 10:14:42 +0900

    Feature/3 child invest (#17)
    * feat getFotfolio,invest
    
    * refartor DTO,remove setter
    
    * feat childInvest
    
    * feat parentGetPortfolio
    
    * feat everyChild
    
    * feat cors

--- isol/server-common/src/main/java/shinhan/server_common/global/security/secret/Secret.java
+++ isol/server-common/src/main/java/shinhan/server_common/global/security/secret/Secret.java
@@ -1,16 +1,16 @@
-//package shinhan.server_common.global.security.secret;
-//
-//import io.jsonwebtoken.io.Decoders;
-//import io.jsonwebtoken.security.Keys;
-//
-//import javax.crypto.SecretKey;
-//
-//public class Secret {
-//
-//    private static final String JWT_SECRET_KEY = "b6ecbae5b1a8de21c5308f4eff3e9924b44b73c442e69e6530e83cab9da3c700";
-//
-//    public static SecretKey getJwtKey() {
-//        byte[] keyBytes = Decoders.BASE64.decode(JWT_SECRET_KEY);
-//        return Keys.hmacShaKeyFor(keyBytes);
-//    }
-//}
+package shinhan.server_common.global.security.secret;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+
+public class Secret {
+
+    private static final String JWT_SECRET_KEY = "b6ecbae5b1a8de21c5308f4eff3e9924b44b73c442e69e6530e83cab9da3c700";
+
+    public static SecretKey getJwtKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(JWT_SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
