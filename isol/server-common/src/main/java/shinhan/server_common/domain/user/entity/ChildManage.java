84fdeb0b208494e019009d6539b4a5bacd4dcbe5
ye-s-rin <pyr53540@gmail.com>
2024-06-22 08:32:53 +0900

    feat: ChildManage Entity

---
+++ isol/server-common/src/main/java/shinhan/server_common/domain/user/entity/ChildManage.java
@@ -0,0 +1,32 @@
+package shinhan.server_common.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.Check;
+
+@Entity
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Check(constraints = "base_rate >= 0")
+@Table(name = "child_manage")
+public class ChildManage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT UNSIGNED")
+    private int id;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "child_sn", referencedColumnName = "serial_num", nullable = false, unique = true, columnDefinition = "BIGINT UNSIGNED")
+    private long childSn;
+    @Column(name = "base_rate", nullable = false)
+    private float baseRate = 0;
+    @Column(name = "invest_limit", columnDefinition = "MEDIUMINT UNSIGNED")
+    private int investLimit = 999999;
+    @Column(name = "loan_limit", columnDefinition = "MEDIUMINT UNSIGNED")
+    private int loanLimit = 999999;
+}
