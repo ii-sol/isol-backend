42f4b9f823334af745307ad2d881825f104d9c1a
ye-s-rin <pyr53540@gmail.com>
2024-06-22 12:35:56 +0900

    feat: 아이 관리 정보 수정 기능 구현

--- isol/server-common/src/main/java/shinhan/server_common/domain/user/entity/ChildManage.java
+++ isol/server-common/src/main/java/shinhan/server_common/domain/user/entity/ChildManage.java
@@ -1,10 +1,7 @@
 package shinhan.server_common.domain.user.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.annotations.Check;
 import shinhan.server_common.domain.user.dto.ChildManageFindOneResponse;
 
@@ -23,16 +20,19 @@ public class ChildManage {
     private int id;
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "child_sn", referencedColumnName = "serial_num", nullable = false, unique = true, columnDefinition = "BIGINT UNSIGNED")
-    private long childSn;
+    private Child child;
+    @Setter
     @Column(name = "base_rate", nullable = false)
-    private float baseRate = 0;
+    private float baseRate = 0L;
+    @Setter
     @Column(name = "invest_limit", columnDefinition = "MEDIUMINT UNSIGNED")
     private int investLimit = 8300000;
+    @Setter
     @Column(name = "loan_limit", columnDefinition = "MEDIUMINT UNSIGNED")
     private int loanLimit = 8300000;
 
     public ChildManage(Child child) {
-        childSn = child.getSerialNum();
+        this.child = child;
     }
 
     public ChildManageFindOneResponse convertToChildManageFIndOneResponse() {
