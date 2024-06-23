3e0b5efcba27416703b7984371e5ddc43a390841
ye-s-rin <pyr53540@gmail.com>
2024-06-23 11:44:57 +0900

    feat: child - getMission(id)

--- isol/server-child/src/main/java/shinhan/server_child/domain/mission/controller/MissionController.java
+++ isol/server-child/src/main/java/shinhan/server_child/domain/mission/controller/MissionController.java
@@ -1,12 +1,16 @@
 package shinhan.server_child.domain.mission.controller;
 
+import jakarta.security.auth.message.AuthException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shinhan.server_child.domain.mission.dto.MissionFindOneResponse;
 import shinhan.server_child.domain.mission.service.MissionService;
 import shinhan.server_common.global.security.JwtService;
+import shinhan.server_common.global.security.dto.UserInfoResponse;
 import shinhan.server_common.global.utils.ApiUtils;
 
 import static shinhan.server_common.global.utils.ApiUtils.success;
@@ -20,8 +24,20 @@ public class MissionController {
     private final MissionService missionService;
     private JwtService jwtService;
 
-    @GetMapping("")
-    public ApiUtils.ApiResult getMission(){
-        return success(missionService.getMission());
+    @GetMapping("/{id}")
+    public ApiUtils.ApiResult getMission(@PathVariable("id") int id) throws AuthException {
+        UserInfoResponse userInfo = jwtService.getUserInfo();
+        long sn = userInfo.getSn();
+
+        MissionFindOneResponse mission = missionService.getMission(id);
+        if (isMissionOwner(mission, sn)) {
+            throw new AuthException("미션을 조회할 수 권한이 없습니다.");
+        }
+
+        return success(mission);
+    }
+
+    private boolean isMissionOwner(MissionFindOneResponse mission, long sn) {
+        return mission.getChildSn() != sn && mission.getParentsSn() != sn;
     }
 }
--- isol/server-child/src/main/java/shinhan/server_child/domain/mission/service/MissionService.java
+++ isol/server-child/src/main/java/shinhan/server_child/domain/mission/service/MissionService.java
@@ -5,7 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shinhan.server_child.domain.mission.dto.MissionFindOneResponse;
+import shinhan.server_child.domain.mission.entity.Mission;
 import shinhan.server_child.domain.mission.repository.MissionRepository;
+
+import java.util.NoSuchElementException;
 
 @Slf4j
 @Service
@@ -15,8 +18,11 @@ public class MissionService {
 
     private final MissionRepository missionRepository;
 
-    public MissionFindOneResponse getMission() {
-        return missionRepository.findById(1).get().convertToMissionFindOneResponse();
+    public MissionFindOneResponse getMission(int id) {
+        Mission mission = missionRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("미션이 존재하지 않습니다."));
+
+        return mission.convertToMissionFindOneResponse();
     }
 
 }
