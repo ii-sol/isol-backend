56e3b93d1a4e88d26e19f313bec20bd0cd982b8c
ye-s-rin <pyr53540@gmail.com>
2024-06-23 09:15:36 +0900

    feat: MissionFindOneResponse DTO

---
+++ isol/server-child/src/main/java/shinhan/server_child/domain/mission/dto/MissionFindOneResponse.java
@@ -0,0 +1,21 @@
+package shinhan.server_child.domain.mission.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+@Getter
+@AllArgsConstructor
+public class MissionFindOneResponse {
+
+    private int id;
+    private long childSn;
+    private long parentsSn;
+    private String content;
+    private int price;
+    private Timestamp createDate;
+    private Timestamp dueDate;
+    private Timestamp completeDate;
+    private int status;
+}
