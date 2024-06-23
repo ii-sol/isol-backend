959ffaac1e164efb94f4cf1d918befb53fde724e
allllfo <mirae4292@gmail.com>
2024-06-23 15:33:02 +0900

    refactor : 미승인 용돈조르기 내역 createDate추가

--- isol/server-child/src/main/java/shinhan/server_child/domain/allowance/dto/UnAcceptTemporalAllowanceFindAllResponse.java
+++ isol/server-child/src/main/java/shinhan/server_child/domain/allowance/dto/UnAcceptTemporalAllowanceFindAllResponse.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import shinhan.server_child.domain.allowance.entity.TemporalAllowance;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Builder
 @Getter
@@ -16,13 +17,14 @@ public class UnAcceptTemporalAllowanceFindAllResponse {
     private int id;
     private int amount;
     private String name;
-    private LocalDate createDate;
+    private LocalDateTime createDate;
 
     public static UnAcceptTemporalAllowanceFindAllResponse of(TemporalAllowance temporalAllowance, String name){
         return UnAcceptTemporalAllowanceFindAllResponse.builder()
                 .id(temporalAllowance.getId())
                 .amount(temporalAllowance.getPrice())
                 .name(name)
+                .createDate(temporalAllowance.getCreateDate())
                 .build();
     }
 }
