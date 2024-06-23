a0854444f915d5f11926920ab683bf58ce6ee39f
ye-s-rin <pyr53540@gmail.com>
2024-06-23 18:54:59 +0900

    fix: Query문 수정

--- isol/server-child/src/main/java/shinhan/server_child/domain/mission/repository/MissionRepository.java
+++ isol/server-child/src/main/java/shinhan/server_child/domain/mission/repository/MissionRepository.java
@@ -3,24 +3,22 @@ package shinhan.server_child.domain.mission.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
-import shinhan.server_child.domain.mission.dto.MissionFindOneResponse;
 import shinhan.server_child.domain.mission.entity.Mission;
 
 import java.util.List;
 
 public interface MissionRepository extends JpaRepository<Mission, Integer> {
 
-    @Query("SELECT * " +
-            "FROM Mission " +
-            "WHERE 1 = 1" +
-            "AND (status = :s1 OR status = :s2)" +
-            "AND child_sn = :child_sn " +
-            "AND parents_sn = :parents_sn " +
-            "ORDER BY id")
-    List<MissionFindOneResponse> findOngoingMissions(
-            @Param("child_sn") long childSn,
-            @Param("parents_sn") long parentsSn,
+    @Query("SELECT m " +
+            "FROM Mission m " +
+            "WHERE 1 = 1 " +
+            "AND (m.status = :s1 OR m.status = :s2) " +
+            "AND m.childSn = :childSn " +
+            "AND m.parentsSn = :parentsSn " +
+            "ORDER BY m.id")
+    List<Mission> findOngoingMissions(
+            @Param("childSn") long childSn,
+            @Param("parentsSn") long parentsSn,
             @Param("s1") int s1,
             @Param("s2") int s2);
 }
