0752d894116122f2bbcea179098f2970d6b4d808
ye-s-rin <pyr53540@gmail.com>
2024-06-23 09:15:11 +0900

    feat: MissionController

--- isol/server-child/src/main/java/shinhan/server_child/domain/mission/controller/MissionController.java
+++ isol/server-child/src/main/java/shinhan/server_child/domain/mission/controller/MissionController.java
@@ -1,4 +1,27 @@
 package shinhan.server_child.domain.mission.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import shinhan.server_child.domain.mission.service.MissionService;
+import shinhan.server_common.global.security.JwtService;
+import shinhan.server_common.global.utils.ApiUtils;
+
+import static shinhan.server_common.global.utils.ApiUtils.success;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/auth/missions")
 public class MissionController {
+
+    private final MissionService missionService;
+    private JwtService jwtService;
+
+    @GetMapping("")
+    public ApiUtils.ApiResult getMission(){
+        return success(missionService.getMission());
+    }
 }
