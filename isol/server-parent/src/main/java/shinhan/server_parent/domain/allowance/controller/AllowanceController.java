4ecd568bd67062dc743119ad7e421a6563d4f052
allllfo <mirae4292@gmail.com>
2024-06-25 14:26:57 +0900

    feat : MonthlyAllowance 에 정기용돈 관련api 추가

--- isol/server-parent/src/main/java/shinhan/server_parent/domain/allowance/controller/AllowanceController.java
+++ isol/server-parent/src/main/java/shinhan/server_parent/domain/allowance/controller/AllowanceController.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import shinhan.server_common.global.exception.AuthException;
+import shinhan.server_common.global.scheduler.DynamicScheduler;
+import shinhan.server_common.global.scheduler.dto.MonthlyAllowanceScheduleChangeOneRequest;
+import shinhan.server_common.global.scheduler.dto.MonthlyAllowanceScheduleSaveOneRequest;
 import shinhan.server_common.global.security.JwtService;
 import shinhan.server_common.global.utils.ApiUtils;
 import shinhan.server_parent.domain.allowance.dto.MonthlyAllowanceFindAllResponse;
@@ -11,6 +14,7 @@ import shinhan.server_parent.domain.allowance.dto.TemporalAllowanceFindAllRespon
 import shinhan.server_parent.domain.allowance.dto.TotalAllowanceFindAllResponse;
 import shinhan.server_parent.domain.allowance.service.AllowanceService;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static shinhan.server_common.global.utils.ApiUtils.success;
@@ -23,6 +27,8 @@ public class AllowanceController {
 
     private final AllowanceService allowanceService;
     private final JwtService jwtService;
+    private final DynamicScheduler dynamicScheduler;
+    private final SchedulerUtils schedulerUtils;
 
     //부모 - 전체 용돈 내역 조회하기
     @GetMapping("history")
@@ -53,5 +59,52 @@ public class AllowanceController {
         Long loginUserSerialNumber = jwtService.getUserInfo().getSn();
         List<MonthlyAllowanceFindAllResponse> response = allowanceService.findMonthlyAllowances(loginUserSerialNumber, csn);
         return success(response);
+    }
+
+    //부모 정기 용돈 생성하기
+    @PostMapping("monthly")
+    public ApiUtils.ApiResult saveMonthlyAllowance(@RequestBody MonthlyAllowanceScheduleSaveOneRequest request) throws jakarta.security.auth.message.AuthException, AuthException {
+        Long loginUserSerialNumber = jwtService.getUserInfo().getSn();
+        String taskId = makeTaskId(loginUserSerialNumber, request.getChildSerialNumber());
+
+        LocalDateTime createDate = LocalDateTime.now();
+        String cronExpression = schedulerUtils.generateTransmitCronExpression(createDate);
+
+        allowanceService.createMonthlyAllowance(loginUserSerialNumber, createDate, request);
+
+        dynamicScheduler.scheduleTask(taskId, cronExpression, request.getPeriod(),() -> {
+            allowanceService.transmitMoneyforSchedule(loginUserSerialNumber, request.getChildSerialNumber(), request.getAmount());
+        });
+        return success(null);
+    }
+
+    //부모 정기 용돈 변경하기
+    @PostMapping("monthly/change")
+    public ApiUtils.ApiResult changeMonthlyAllowance(@RequestBody MonthlyAllowanceScheduleChangeOneRequest request) throws jakarta.security.auth.message.AuthException, AuthException {
+        Long loginUserSerialNumber = jwtService.getUserInfo().getSn();
+        String taskId = makeTaskId(loginUserSerialNumber, request.getChildSerialNumber());
+
+        String cronExpression = schedulerUtils.retestGenerateCronExpression(request.getDateBeforeChange());
+        allowanceService.changeMonthlyAllowance(loginUserSerialNumber, request);
+
+        dynamicScheduler.rescheduleTask(taskId, cronExpression, request.getPeriod(),() -> {
+            allowanceService.transmitMoneyforSchedule(loginUserSerialNumber, request.getChildSerialNumber(), request.getAmount());
+        });
+        return success(null);
+    }
+
+    //부모 정기 용돈 해제하기
+    @DeleteMapping()
+    public ApiUtils.ApiResult deleteMonthlyAllowance(@RequestParam("monthlyId") Integer monthlyId, @RequestParam("csn") Long csn) throws jakarta.security.auth.message.AuthException, AuthException {
+        Long loginUserSerialNumber = jwtService.getUserInfo().getSn();
+        String taskId = makeTaskId(loginUserSerialNumber, csn);
+
+        allowanceService.deleteMonthlyAllowance(monthlyId);
+        dynamicScheduler.stopScheduledTask(taskId);
+        return success(null);
+    }
+
+    private static String makeTaskId(Long loginUserSerialNumber, Long childSerialNumber) {
+        return loginUserSerialNumber.toString() + childSerialNumber.toString();
     }
 }
