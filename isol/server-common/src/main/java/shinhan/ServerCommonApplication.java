2b31fbab646272f40616f04009cb6c0276e62288
ye-s-rin <46209669+ye-s-rin@users.noreply.github.com>
2024-06-25 16:22:27 +0900

    Refactor/21 mission - DB 세팅 (#91)
    * feat: DB Config
    
    * refactor: @Table 네임 지정
    
    * refactor: 사용하지 않는 annotation 제거
    
    * refactor: 패키지 경로 수정
    
    * refactor: 패키지 경로 수정
    
    * refactor: 사용하지 않는 annotation 삭제

--- isol/server-common/src/main/java/shinhan/ServerCommonApplication.java
+++ isol/server-common/src/main/java/shinhan/ServerCommonApplication.java
@@ -2,18 +2,8 @@ package shinhan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import shinhan.server_common.notification.mongo.NotificationRepository;
 
-// common
 @SpringBootApplication
-@EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(
-    type = FilterType.ASSIGNABLE_TYPE,
-    classes = NotificationRepository.class))
-@EnableMongoRepositories(basePackages = {"shinhan.server_common.notification.mongo"})
 public class ServerCommonApplication {
 
     public static void main(String[] args) {
