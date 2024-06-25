c05bcc9ccdad319b298bd0052e4bf227ed1aad11
allllfo <mirae4292@gmail.com>
2024-06-26 01:04:28 +0900

    refactor : 부모 정기 용돈 관련 query 변경

--- isol/server-parent/src/main/java/shinhan/server_parent/domain/allowance/repository/MonthlyAllowanceRepositoryCustom.java
+++ isol/server-parent/src/main/java/shinhan/server_parent/domain/allowance/repository/MonthlyAllowanceRepositoryCustom.java
@@ -5,7 +5,7 @@ import shinhan.server_parent.domain.allowance.entity.MonthlyAllowance;
 import java.util.List;
 
 public interface MonthlyAllowanceRepositoryCustom {
-    List<MonthlyAllowance> findByUserSerialNumberAndCreateDate(Long userSerialNumber, Integer year, Integer month, Long csn);
+//    List<MonthlyAllowance> findByUserSerialNumberAndCreateDate(Long userSerialNumber, Integer year, Integer month, Long csn);
 
-    List<MonthlyAllowance> findByParentsSerialNumberAndChildrenSerialNumberAndStatus(Long userSerialNumber, Long csn, Integer status);
+//    List<MonthlyAllowance> findByParentsSerialNumberAndChildrenSerialNumberAndStatus(Long userSerialNumber, Long csn, Integer status);
 }
