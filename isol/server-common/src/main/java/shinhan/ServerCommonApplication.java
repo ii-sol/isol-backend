46225bd1a2b79a7f8ac9a542103d6dca645f9a5c
ye-s-rin <pyr53540@gmail.com>
2024-06-25 16:16:24 +0900

    refactor: 사용하지 않는 annotation 삭제

--- isol/server-common/src/main/java/shinhan/ServerCommonApplication.java
+++ isol/server-common/src/main/java/shinhan/ServerCommonApplication.java
@@ -2,11 +2,8 @@ package shinhan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-// common
 @SpringBootApplication
-@EnableJpaRepositories
 public class ServerCommonApplication {
 
     public static void main(String[] args) {
