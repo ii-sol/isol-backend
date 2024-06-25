cd9a828c8b209f8012de612facf47094ef7a37af
kim hyun soo <hyjkl0124@naver.com>
2024-06-26 01:53:13 +0900

    no message

--- isol/server-child/src/main/java/shinhan/ServerChildApplication.java
+++ isol/server-child/src/main/java/shinhan/ServerChildApplication.java
@@ -1,10 +1,18 @@
 package shinhan;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerChildApplication {
+    @Autowired
+    private final RabbitTemplate rabbitTemplate ;
+
+    public ServerChildApplication(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerChildApplication.class, args);
