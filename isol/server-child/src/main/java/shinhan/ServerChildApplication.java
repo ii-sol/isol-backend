6731efd3c7d55e62dded316adfaee717aa9cc7b6
NOEL-code <junusong12@naver.com>
2024-06-26 00:25:59 +0900

    feat: 배포환경 시작할거임

--- isol/server-child/src/main/java/shinhan/ServerChildApplication.java
+++ isol/server-child/src/main/java/shinhan/ServerChildApplication.java
@@ -1,42 +1,15 @@
 package shinhan;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
-@SpringBootApplication(scanBasePackages = {"shinhan.server_child", "shinhan.server_common"})
-@EnableJpaRepositories(basePackages={"shinhan.server_child","shinhan.server_common.domain"})
-@EnableMongoRepositories(basePackages = {"shinhan.server_common.notification.mongo"})
-public class ServerChildApplication implements CommandLineRunner {
+@SpringBootApplication
+public class ServerChildApplication {
 
-    private final RabbitTemplate rabbitTemplate ;
-
-    public ServerChildApplication(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerChildApplication.class, args);
-    }
-
-    @Bean
-    public Queue queue(@Value("${rabbitmq.queue}") String queueName) {
-        return new Queue(queueName, false);
-    }
-
-    @Value("${rabbitmq.queue}")
-    private String queueName;
-
-    @Override
-    public void run(String... args) throws Exception {
-        rabbitTemplate.convertAndSend(queueName, "Hello from Producer!");
     }
 
 }
--- isol/server-parent/src/main/java/shinhan/ServerParentApplication.java
+++ isol/server-parent/src/main/java/shinhan/ServerParentApplication.java
@@ -2,23 +2,12 @@ package shinhan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 @SpringBootApplication
 public class ServerParentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerParentApplication.class, args);
-    }
-
-    @RabbitListener(queues = "hello")
-    public void listen(String message) {
-        System.out.println("Received: " + message);
     }
 
 
