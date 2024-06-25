7867b26c5971fb295baa5f071b0b54fdef3bd4d2
ye-s-rin <pyr53540@gmail.com>
2024-06-25 15:53:00 +0900

    refactor: 사용하지 않는 annotation 제거

--- isol/server-child/src/main/java/shinhan/ServerChildApplication.java
+++ isol/server-child/src/main/java/shinhan/ServerChildApplication.java
@@ -2,12 +2,8 @@ package shinhan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication(scanBasePackages = {"shinhan.server_child", "shinhan.server_common"})
-@EnableJpaRepositories(basePackages={"shinhan.server_child","shinhan.server_common.domain"})
-@EnableMongoRepositories(basePackages = {"shinhan.server_common.notification.mongo"})
+@SpringBootApplication
 public class ServerChildApplication {
 
     public static void main(String[] args) {
