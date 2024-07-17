a3cab69222caf1b82b42f46642e494c5934546a4
yerin <pyr53540@gmail.com>
2024-07-17 18:16:51 +0900

    refactor: ParentsUpdateRequest와 ChildUpdateRequest 통합

--- isol/server-common/src/main/java/shinhan/server_common/domain/user/dto/UserUpdateRequest.java
+++ isol/server-common/src/main/java/shinhan/server_common/domain/user/dto/UserUpdateRequest.java
@@ -12,19 +12,19 @@ import java.sql.Date;
 
 @RequiredArgsConstructor
 @Getter
-public class ParentsUpdateRequest {
+public class UserUpdateRequest {
 
     @Setter
     private long serialNum;
     @NotBlank(message = "전화번호를 입력해주세요.")
     @Pattern(regexp = "\\d{3}-\\d{4}-\\d{4}", message = "전화번호 형식이 올바르지 않습니다.")
-    private  String phoneNum;
+    private final String phoneNum;
     @NotBlank(message = "이름을 입력해주세요.")
     @Pattern(regexp = "^[가-힣]{2,5}$", message = "이름은 한글로 최소 2글자 최대 5글자까지 입력 가능합니다.")
-    private  String name;
+    private final String name;
     @NotNull(message = "생일을 입력해주세요.")
     @Past(message = "생일은 현재 날짜보다 이전이어야 합니다.")
-    private  Date birthDate;
+    private final Date birthDate;
     @NotNull(message = "프로필 번호를 입력해주세요.")
-    private  int profileId;
+    private final int profileId;
 }
